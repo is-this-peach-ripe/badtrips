9089f92105c7de9853b20b1a45c162a35bc68157
Gonçalo Grilo <grilogoncalo31@ua.pt>
2018-04-13 20:00:54 +0100

    project structure

---
+++ app.py
@@ -0,0 +1,83 @@
+from flask import Flask, session, request
+import redis
+import yelp_stuff
+app = Flask(__name__)
+app.secret_key = ''
+
+@app.route('/')
+def index():
+    '''
+    Starting page
+    user chooses the location of the game
+    :return:
+    '''
+    pass
+
+@app.route('/play', methods=['POST'])
+def play():
+    '''
+    Must recieve a location in the request
+    if we dont have a location generate a random one?
+
+    :return:
+    '''
+    id = newGame(request.form['location'])
+    session['id'] = id
+
+    # return play page and call /newquestion
+    pass
+
+@app.route('/newquestion', methos=['POST'])
+def newQuestion():
+    '''
+    Create a new question from yelp
+    returns a json with:
+     id: this question id, to check the correct responce
+     A:
+     B:
+    :return:
+    '''
+    game = getGame(session['id'])
+    question, answer = yelp_stuff.createNewQuestion(game['location'])
+    store_answer(answer, game)
+    #return question
+    pass
+
+@app.route('/answer', methods=['POST'])
+def answer():
+    '''
+    will recieve answer:<nome>
+    :return:
+    '''
+    answer = request.form['answer']
+    game = getGame(session['id'])
+
+    responce = check_answer(answer, game)
+    #devolve resposta correcta
+    pass
+
+def newGame(location):
+    '''
+    create a new game that has a location, username, id and score
+    :return: session id
+    '''
+    pass
+
+def getGame(id):
+    '''
+    get a game session from redis
+    :param id:
+    :return:
+    '''
+
+def store_answer(answer, game):
+    '''
+    store a answer for a game
+    :param answer:
+    :param game:
+    :return:
+    '''
+    pass
+
+def check_answer(answer, game):
+    pass
---
+++ yelp_stuff.py
@@ -0,0 +1,4 @@
+
+
+def createNewQuestion(location):
+    pass
