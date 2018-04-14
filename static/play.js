0803348cdd2975c8a14824243d4a6e8d31861f01
hugofragata <hugofragata@ua.pt>
2018-04-14 18:27:30 +0100

    boy yall be wilding out like some mf fools like yall on crack

--- static/play.js
+++ static/play.js
@@ -4,7 +4,10 @@ newQuestion();
 
 function post_answer(bt) {
     console.log(bt);
-    $.ajax("/answer", {dataType:"json", method:"POST", data:{answer: bt.value}}).done(function (data) {
+    var value;
+    if(bt=='a') value = valueA;
+    else value = valueB;
+    $.ajax("/answer", {dataType:"json", method:"POST", data:{answer: value}}).done(function (data) {
         console.log(data);
         if (data['correct'] === false ){
             window.alert("gameover\nfinal score: " + score);
@@ -27,14 +30,19 @@ function newQuestion() {
         console.log(data);
     var a_url = data['A'].image_url;
     var b_url = data['B'].image_url;
+    valueA = data['A'].name;
+    valueB = data['B'].name;
     $('#imgA').attr("src", a_url);
     $('#imgB').attr("src", b_url);
     $("#score").html(score);
-    $('#nomeA').html(data['A'].name);
-    $('#A').val(data['A'].name);
-    $('#nomeB').html(data['B'].name);
-    $('#B').val(data['B'].name);
+    $('#nomeA').html(valueA);
+    $('#A').val(valueA);
+    $('#nomeB').html(valueB);
+    $('#B').val(valueB);
     }).fail(function () {
         console.log("erro");
     });
 }
+
+var valueA;
+var valueB;
