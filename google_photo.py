82df9e07df8be652fc440d524dda26b7b0c5bae6
Gonçalo Grilo <grilogoncalo31@ua.pt>
2018-04-15 16:14:20 +0100

    add timeout and ignore photos from google

--- google_photo.py
+++ google_photo.py
@@ -3,13 +3,16 @@ import random
 import os.path
 
 KEY = "AIzaSyDAciMKDEcZASMZ1IkCr73wYycJxDcG1lY"
-c = googlemaps.Client(key=KEY)
+c = googlemaps.Client(key=KEY, timeout=2)
 
 
 def get_photo(name, coord):
     fname = "static/" + name.replace(" ", "") + ".jpeg"
     if os.path.isfile(fname):
+        print("from cache")
         return fname
+    else:
+        return "/static/burger.jpg"
     photo = None
     r = c.places(name, location=coord, radius=50000)
     #print(r['results'])
@@ -22,7 +25,7 @@ def get_photo(name, coord):
         print("No photos...")
         return "/static/burger.jpg"
     else:
-        photo = c.places_photo(photo['photo_reference'], max_width=5000, max_height=5000)
+        photo = c.places_photo(photo['photo_reference'], max_width=1000, max_height=1000)
         f = open(fname, 'wb')
         for chunk in photo:
             if chunk:
